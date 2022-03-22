206eaecb4a68fa0c7bbf6e616d091f9edf258c0b
Ciro Rufino <99320626+cirorufino@users.noreply.github.com>
2022-03-22 18:32:27 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require ('express');
 const app = express();
 const cors = require ('cors');
-// app.engine('html', handle.engine({extname: '.hbs'}));
-// app.set('view engine', 'html');
+
 
 app.use(express.json());
 app.use(cors());
